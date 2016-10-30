6a7041c70729a5b3882a292ac8b19ada05cb8a1f
Orlando Morales <orlando@ibuildweb.site>
2016-10-29 23:45:21 -0400

    Finished every view except Deploy

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
+'use strict';
+
 //Close mobile nav menu after clicking a link
-            $(document).on('click','.navbar-collapse.in',function(e) {
-                if( $(e.target).is('a') ) {
-                    $(this).collapse('hide');
-                }
-            });
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+$(document).ready(function() {
+
+
+
+});
