70dfa9f727653a0a41d71fae5ede15e57440476e
Orlando <omorales@aigaxis.com>
2017-03-20 09:44:07 -0400

    Highlight experiments

--- public/app/app.js
+++ public/app/app.js
@@ -6,26 +6,32 @@ app.config(function($routeProvider) {
     $routeProvider
         .when('/home', {
             templateUrl : 'views/home.html', 
+            controller: 'mainController'
         })
         
        .when('/nodes', {
             templateUrl : 'views/nodes.html',
+            controller: 'mainController'
         }) 
         
         .when('/tools', {
             templateUrl : 'views/tools.html',
+            controller: 'mainController'
         }) 
         
         .when('/work', {
             templateUrl : 'views/work.html',
+            controller: 'mainController'
         }) 
         
         .when('/learn', {
             templateUrl : 'views/learn.html',
+            controller: 'mainController'
         }) 
         
         .when('/deploy', {
             templateUrl : 'views/deploy.html',
+            controller: 'mainController'
         }) 
         
         .otherwise({redirectTo:'/home'})
@@ -34,13 +40,49 @@ app.config(function($routeProvider) {
 
 app.controller('mainController', function($scope) {
 	$scope.title = 'Dev.iBuildWeb.Solutions';
+    $scope.positionHighlight = function() {
+        console.log('Position changed.');
+        if(document.getElementById('selectPosition').value === 'Developer'){
+            console.log('Developer selected.');
+            var dev = getElementByClassName('developer');
+            dev.className += 'highlight';
+            
+        }else if(document.getElementById('selectPosition').value === 'Digital Marker') {
+            console.log('Digital marketer selected.');
+        };
+    };
 });
 
 //Toggles job field selection skill highlights
 
-$( "#selectPosition" ).change(function() {
-  alert( "Handler for .change() called." );
+/*
+$(document).on('change','#selectPosition',function() {
+   console.log('Changing position'); 
+   if($(this).val === 'Developer'){
+       console.log('Developer selected.');
+	   $('p.marketer').removeClass('highlight');
+	   $('p.developer').addClass('highlight');
+	}else if($(this).val === 'Digital Marker') {
+		$('p.developer').addClass('highlight');
+		$('p.marketer').addClass('highlight');
+	}
 });
+*/
+
+/*
+$( '#selectPosition' ).on('change', function() {
+    console.log('Changing position');
+    if($(this).val === 'Developer'){
+       console.log('Developer selected.');
+	   $('.marketer').removeClass('highlight');
+	   $('.developer').addClass('highlight');
+	}else if($(this).val === 'Digital Marker') {
+		$('.developer').addClass('highlight');
+		$('.marketer').addClass('highlight');
+	}
+});
+*/
+
 /*
 $(document).on('change', '#selectPosition', function() { 
 	if($(this).val === 'Developer'){
